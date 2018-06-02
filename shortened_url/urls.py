e79b22d8c20f1771b3482728c2a0b09c5228e5e7
foonia <ljh9032a@naver.com>
2018-06-02 20:00:56 +0900

    implement Redirect Url & process of exception

--- shortened_url/urls.py
+++ shortened_url/urls.py
@@ -2,9 +2,10 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [ 
-    re_path(r'^target/(?P<target_url>.+)/$', views.TargetUrlAPIView.as_view(), 
+   re_path(r'^target/(?P<target_url>.+)/$', views.TargetUrlAPIView.as_view(), 
         name='target-url'),
     re_path(r'^short/(?P<short_url>.+)/$', views.ShortUrlAPIView.as_view(), 
         name='short-url'),
-
+   re_path(r'^(?P<redirect_url>.+)/$', views.RedirectUrlAPIView.as_view(),
+        name='redirect-url'),
 ]
--- shortened_url/views.py
+++ shortened_url/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import serializers
 from django.core import exceptions
 from django.core.validators import URLValidator
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
+from django.conf import settings
 from .models import Url
 from .validations import validate
 
@@ -12,14 +14,19 @@ class TargetUrlAPIView(APIView):
         대상 url과 함께 요청 시 단축 url을 반환
         """
         url_validator = URLValidator() 
-        url_validator(target_url)
+
+        try:
+            url_validator(target_url)
+        except exceptions.ValidationError:
+            raise serializers.ValidationError
 
         try:
             entry = Url.objects.get(origin_url=target_url)
         except exceptions.ObjectDoesNotExist:
             entry = Url.create(target_url)
 
-        return Response(request.META['HTTP_HOST'] + '/' + entry.shortened_url, status=201)
+
+        return Response(settings.DOMAIN + entry.shortened_url, status=201)
 
 
 class ShortUrlAPIView(APIView):
@@ -27,9 +34,9 @@ class ShortUrlAPIView(APIView):
     단축 url과 함께 요청 시 해당 단축 url 접속 횟수 반환
     """
     def get(self, request, short_url):
-        http_host = request.META['HTTP_HOST']
+        http_host = settings.DOMAIN
         index = short_url.find(http_host)
-        slice_short_url = short_url[index + len(http_host) + 1:]
+        slice_short_url = short_url[index + len(http_host):]
         entry = get_object_or_404(Url, shortened_url=slice_short_url)
         
         return Response(entry.count, status=200)
@@ -39,9 +46,9 @@ class ShortUrlAPIView(APIView):
     단축 url과 함께 요청 시 지정 단축 url을 삭제
     """
     def delete(self, request, short_url):
-        http_host = request.META['HTTP_HOST']
+        http_host = settings.DOMAIN
         index = short_url.find(http_host)
-        slice_short_url = short_url[index + len(http_host) + 1:]
+        slice_short_url = short_url[index + len(http_host):]
         entry = get_object_or_404(Url, shortened_url=slice_short_url)
         entry.delete()
         return Response(status=204)
@@ -49,5 +56,15 @@ class ShortUrlAPIView(APIView):
 
 
 class RedirectUrlAPIView(APIView):
-    def get(self, request):
-        pass
+    """
+    단축 url 접속 시 원래 url로 Redirect (접속 횟수 카운트)
+    """
+    def get(self, request, redirect_url):
+        entry = get_object_or_404(Url, shortened_url=redirect_url)
+        entry.count += 1
+        entry.save()
+        return redirect(entry.origin_url)
+
+
+
+
